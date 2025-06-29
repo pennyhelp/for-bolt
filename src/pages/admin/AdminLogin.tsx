cf0f1bd95f5ddb382482a704f8fdcf7ec27e3403
pennyhelp <pennyekarthelpdesk@gmail.com>
2025-06-30 01:07:54 +0530

    Update AdminLogin.tsx

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -8,7 +8,9 @@ import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
 import { Label } from '../../components/ui/label';
 import { Lock, User, AlertCircle, CheckCircle, Database, RefreshCw } from 'lucide-react';
-import { toast } from '@/hooks/use-toast';
+import { toast } from '../../hooks/use-toast';
+
+
 
 const AdminLogin = () => {
   const navigate = useNavigate();
